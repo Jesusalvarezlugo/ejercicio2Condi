d5f0ce34be257f614d3a80784d0fb14d2d7cdca8
Jesús <csi22-jalvugo@colegioaltair.net>
2023-10-24 12:31:20 +0200

    Agregar archivos de proyecto.

---
+++ controladores/Program.cs
@@ -0,0 +1,26 @@
+﻿using ejercicio2Condi.servicios;
+
+namespace ejercicio2Condi.controladores
+{
+    class Program
+    {
+        internal static void Main(string[] args)
+        {
+            int numero;
+            numeroInterfaz mi  = new numeroImplementacion();
+
+            numero = mi.PedirNumero();
+            int raiz = (int)Math.Sqrt(numero);
+
+            if (numero == raiz*raiz)
+            {
+                Console.WriteLine("La raiz del numero {0} es correcta",numero);
+            }
+
+            else
+            {
+                Console.WriteLine("La raiz del numero {0} no es correcta",numero);
+            }
+        }
+    }
+}
---
+++ servicios/numeroImplementacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2Condi.servicios
+{
+    internal class numeroImplementacion : numeroInterfaz
+    {
+        public int PedirNumero()
+        {
+            int numero;
+
+            Console.WriteLine("Introduce un número: ");
+            numero = Int32.Parse(Console.ReadLine());
+
+            return numero;
+        }
+    }
+}
